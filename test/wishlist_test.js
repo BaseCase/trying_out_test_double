5c865f3879fcf29bd41a7b57bc8ae00c6df445d8
Casey Brant <casey@caseybrant.com>
2015-09-10 11:30:21 -0500

    use testdouble lib in Wishlist tests instead of object literal

--- test/wishlist_test.js
+++ test/wishlist_test.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var td = require('testdouble');
 
 var wishlist = require('../src/wishlist');
 var Wishlist = wishlist.Wishlist;
@@ -10,13 +11,13 @@ describe('Wishlist', function() {
   });
 
   it("can add items", function() {
-    var stub = {};
+    var stub = td.create('item');
     this.subject.add_item(stub);
     expect(this.subject.items[this.subject.items.length - 1]).to.equal(stub);
   });
 
   it("can remove items", function() {
-    var stub = {};
+    var stub = td.create('item');
     this.subject.items = [stub];
     this.subject.remove_item(0);
     expect(this.subject.items.length).to.equal(0);
